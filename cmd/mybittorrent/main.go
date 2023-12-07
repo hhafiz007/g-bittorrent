2ea2eadbafb974d9d9061166ce818fafa039b164
Hamza Hafiz <hamzaha2@buffalo.edu>
2023-12-07 00:32:40 -0500

    pass 2nd stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -34,7 +34,7 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
 	}else if (bencodedString[0]) == 'i' {
 		end := len(bencodedString)
-		return bencodedString[1 :end-1], nil
+		return int(bencodedString[1 :end-1]), nil
 		
 
 	}else {
