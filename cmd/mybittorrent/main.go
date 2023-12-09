43efe4748f3beab536e42a761483da9b7dd8c0a2
Hamza Hafiz <hamzaha2@buffalo.edu>
2023-12-09 01:39:47 -0500

    pass 1st stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -59,6 +59,7 @@ func decodeList(bencodedString string,idx int) (interface{}, int,error){
 
 	for bencodedString[i] != 'e' {
 		decoded, newIdx,_ := decodeBencode(bencodedString[i:],i)
+		fmt.Println("hello there",decoded)
 		slice = append(slice,decoded)
 		i = newIdx+1
 		idx = i
